87221508d1e43738c621ef98412b32daf6910217
Yevgeniy Valdman <evaldman@yahoo.com>
2020-12-03 17:17:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(emoticons_file, emot)
   end
   return "Sorry, that emoticon was not found"
 end
-puts get_japanese_emoticon("./lib/emoticons.yml",  :$)
+puts get_japanese_emoticon("./lib/emoticons.yml", ":$")
 
 def get_english_meaning(emoticons_file, emot)
   meaning = load_library(emoticons_file)
